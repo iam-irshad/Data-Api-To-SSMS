587318d8915e5da66ac4c15fea1bdaa45cf1313c
irshad <iamirshadansari.24@gmail.com>
2024-01-30 14:59:40 +0530

    extract data from url

---
+++ extract_data_from_url.py
@@ -0,0 +1,12 @@
+def get_data_from_url(apikey,url):
+    params = {
+        "apikey":apikey
+    }
+    response = requests.get(url=url,params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Error fetching data. Status code: {response.status_code}")
+
+print(get_data_from_url("YdH3zeYkA1CxeAUlU8AJWhMIyAEKF2ePJVBCzoJC","https://api.currencyapi.com/v3/currencies?"))
